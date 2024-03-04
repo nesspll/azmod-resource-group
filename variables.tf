ac08373e8be4936868c6637920c6718a25a727f0
nesspll <nesim@cuburn.com>
2024-03-05 00:20:37 +0100

    adding nulls

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+  default = null
 }
 
 variable "resource_group_location" {
   description = "Location of the Resource Group"
   type        = string
+  default = null
 }
 
 variable "environment" {
   description = "Environment name for deployment"
   type        = string
+  default = null
 }
