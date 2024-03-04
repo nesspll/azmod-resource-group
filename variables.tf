3cccc09e8a5aac369adaad8c6de16094a71641a4
nesspll <nesim@cuburn.com>
2024-03-05 00:32:26 +0100

    up

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default = ""
+  default = "dummy"
 }
 
 variable "resource_group_location" {
   description = "Location of the Resource Group"
   type        = string
-  default = ""
+  default = "eastus"
 }
 
 variable "environment" {
   description = "Environment name for deployment"
   type        = string
-  default = ""
+  default = "qa"
 }
